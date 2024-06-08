e0fa1fa05605ee287eae89dd9ae5e33f7ca6bc74
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-06-08 23:37:20 +0200

    additemToCart added with localstore

--- src/components/BurgerCard.jsx
+++ src/components/BurgerCard.jsx
@@ -1,30 +1,46 @@
-import { json } from "react-router-dom";
 import burger from "../assets/images/foods/food_image_1.svg";
-import { parse } from "postcss";
 
 function BurgerCard({ bg, item }) {
   const imageStyle = `w-full bg-${bg ? "sand" : ""} rounded-[10px]`;
-
+  let cartItem = {
+    item_id: item.id,
+    count: 1,
+  };
   function addItemToLocalCart() {
-    let jsonValue = localStorage.getItem("cart");
-    //om det inte finns en cart
-    if (!jsonValue) {
-      localStorage.setItem("cart", JSON.stringify(item));
-    } else {
-      let parsedValue = JSON.parse(jsonValue);
-      let cart = [];
-      if (parsedValue.length > 1) {
-        cart.push(...parsedValue);
-      } else {
-        console.log(parsedValue);
-        cart.push(parsedValue);
+    let item_is_added = false;
+    if (localStorage.getItem("cart")) {
+      console.log("finns");
+      let res = localStorage.getItem("cart");
+      let cart = JSON.parse(res);
 
+      cart.map((i) => {
+        parseInt(i.item_id) == parseInt(item.id)
+          ? increaseItemCount(i, true)
+          : i;
+      });
+
+      console.log(item_is_added);
+      if (item_is_added == false) {
+        console.log("item ska läggas till");
+        cart.push(cartItem);
         console.log(cart);
       }
-      cart.push(item);
-
       localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+      console.log("finns ej");
+
+      localStorage.setItem("cart", JSON.stringify([cartItem]));
     }
+
+    function increaseItemCount(item, type) {
+      type ? item.count++ : item.count--;
+      item_is_added = true;
+    }
+
+    function updateCart() {}
+    //annars om det finns en cart, parse:a carten och kolla om objekt med item.id = id jag vill lägga till, isåfall öka det objektets count med 1
+    //om det inte finns något item med det id:et, kopiera carten och sätt till en ny version av carten, och pusha ett nytt objekt med id och count
+    //skriv över den dåvarande carten med den nya carten, med att stringify:a den
   }
   return (
     <div className="text-white bg-midnightblack bg-opacity-70 drop-shadow-md rounded-[23px] cursor-pointer">
